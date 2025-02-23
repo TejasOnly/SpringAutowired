2fbd44ed66f17ecefb03a89b94977ab03168a1ac
TejasOnly <143946996+TejasOnly@users.noreply.github.com>
2025-02-23 11:41:01 +0530

    Create Subjects.java
    List of Subjects

---
+++ Subjects.java
@@ -0,0 +1,28 @@
+package com.ty.beans;
+
+import java.util.List;
+
+public class Subjects {
+	
+	private List<String> Subjects;
+	
+	public List<String> getSubjects() {
+		return Subjects;
+	}
+
+	public void setSubjects(List<String> subjects) {
+		Subjects = subjects;
+	}
+
+	@Override
+	public String toString() {
+		return Subjects.toString();
+	}
+	
+	
+
+	
+	
+	
+
+}
